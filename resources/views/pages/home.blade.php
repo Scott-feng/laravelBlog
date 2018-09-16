804dd03fb1611c866f4b4207fa37f5ad6f6cc0a2
vagrant <vagrant@homestead>
2018-09-16 12:47:49 +0000

    home page modify

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,17 @@
 
 @section('content')
     <div class="col-lg-8">
-        {{--<img src="{{ config('app.url') }}/image/banner3.jpg" alt="">--}}
+        <div class="layui-carousel" id="test1">
+        <div carousel-item>
+            <div>
+                <img src="{{ config('app.url') }}/image/banner3.jpg" alt="">
+            </div>
+            <div>
+                <img src="{{ config('app.url') }}/image/banner4.jpg" alt="">
+            </div>
+        </div>
+        {{--<img src="{{ config('app.url') }}/image/banner3.jpg" alt="" style="width: 700">--}}
+    </div>
     </div>
 
     <div class="col-lg-4">
@@ -37,26 +47,26 @@
 
 @section('script')
     <script src="{{ config('app.url')}}/X-admin/lib/layui/layui.js"></script>
-    {{--<script>--}}
-        {{--layui.use('carousel', function(){--}}
-            {{--var carousel = layui.carousel;--}}
-            {{--//建造实例--}}
-            {{--carousel.render({--}}
-                {{--elem: '#test1'--}}
-                {{--,width: '100%' //设置容器宽度--}}
-                {{--,arrow: 'always' //始终显示箭头--}}
-                {{--// ,anim: 'updown' //切换动画方式--}}
-                {{--,interval: 7000--}}
-                {{--,height:'400px'--}}
+    <script>
+        layui.use('carousel', function(){
+            var carousel = layui.carousel;
+            //建造实例
+            carousel.render({
+                elem: '#test1'
+                ,width: '100%' //设置容器宽度
+                ,arrow: 'always' //始终显示箭头
+                // ,anim: 'updown' //切换动画方式
+                ,interval: 7000
+                ,height:'400px'
 
-            {{--});--}}
-        {{--});--}}
-        {{--layui.use('flow', function(){--}}
-            {{--var flow = layui.flow;--}}
-            {{--//信息流--}}
+            });
+        });
+        layui.use('flow', function(){
+            var flow = layui.flow;
+            //信息流
 
-            {{--//图片懒加载--}}
-            {{--flow.lazyimg();--}}
-        {{--});--}}
-    {{--</script>--}}
+            //图片懒加载
+            flow.lazyimg();
+        });
+    </script>
 @endsection
