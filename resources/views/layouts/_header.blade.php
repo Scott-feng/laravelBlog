5b9c61626540f24084f33ef9adb28197d1fff951
scott-feng <909698516@qq.com>
2018-09-17 13:21:24 +0800

    add middleware

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -46,9 +46,9 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                @role('Founder')
+                                @if(Auth::user()->is_admin)
                                 <span class="badge pull-right">VIP</span>
-                                @endrole
+                                @endif
                                 <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
@@ -79,13 +79,13 @@
                                 </a>
                             </li>
                             {{--only admin user will see--}}
-                            @role('Founder')
+                            @if(Auth::user()->is_admin)
                             <li>
                                 <a href="{{ route('admin') }}">
                                     后台管理
                                 </a>
                             </li>
-                            @endrole
+                            @endif
                         </ul>
                     </li>
                 @else
